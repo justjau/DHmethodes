ba5df91de1f1a3b94874fbf904c52c7194c9475d
justjau <34780175+justjau@users.noreply.github.com>
2017-12-22 23:40:36 +0200

    Add files via upload

---
+++ frequency.py
@@ -0,0 +1,19 @@
+from textfile_io import write_lines_into_txt_file
+
+import re
+import string
+frequency = {}
+document_text = open('teikas2-txt.txt', 'r')
+text_string = document_text.read().lower()
+match_pattern = re.findall(r'\b[a-z]{3,15}\b', text_string)
+ 
+for word in match_pattern:
+    count = frequency.get(word,0)
+    frequency[word] = count + 1
+     
+frequency_list = frequency.keys()
+ 
+for words in frequency_list:
+    print (words, frequency[words])
+
+  
